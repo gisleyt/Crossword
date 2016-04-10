b483b286387aab6f0c84936e20ebc29ec215b3c3
Gisle Ytrestol <gisle.ytrestol@cxense.com>
2016-04-10 13:26:37 +0200

    Associate frequency and return dictionary from trie
    Associate frequency with words.
    Return full dictionary + freq as stream.

--- src/com/cuteforce/scala/crossword/Node.scala
+++ src/com/cuteforce/scala/crossword/Node.scala
@@ -3,19 +3,21 @@ package com.cuteforce.scala.crossword
 import scala.annotation.tailrec
 import scala.collection.mutable
 
-class Node(val letter : Char) {
-   val daughters = mutable.Map[Char, Node]() 
-   
-   def this() {
-      this('%')
-   }
+class Node(val letter : Option[Char]) {
+
+  val daughters = mutable.Map[Char, Node]()
+  var frequency = 0
+
+  def this() {
+    this(None)
+  }
 
   @tailrec final def getNode(prefix : Array[Char]) : Option[Node] = {
     val nodeOption = this.daughters.get(prefix.head)
-     if (!nodeOption.isDefined || prefix.length == 1) {
-       return nodeOption
-     } else {
-       return nodeOption.get.getNode(prefix.tail)
-     }
+    if (!nodeOption.isDefined || prefix.tail.isEmpty) {
+      return nodeOption
+    } else {
+      return nodeOption.get.getNode(prefix.tail)
+    }
   }
 }
--- src/com/cuteforce/scala/crossword/Trie.scala
+++ src/com/cuteforce/scala/crossword/Trie.scala
@@ -2,27 +2,58 @@ package com.cuteforce.scala.crossword
 
 import scala.annotation.tailrec
 
+
 object Trie {
-  
+
   @tailrec final def inject(node: Node, word : Array[Char]): Unit = {
     if (!word.isEmpty) {
       if (!node.daughters.contains(word.head)) {
-        node.daughters.put(word.head, new Node(word.head))
+        node.daughters.put(word.head, new Node(Some(word.head)))
       }
       inject(node.daughters(word.head), word.tail)
     } else {
-      node.daughters.put('.', new Node())
+      node.frequency += 1
     }
   }
-  
+
+
+  final def getWords(node: Node, prefix: Array[Char]): Stream[String] = {
+    if (node.letter.isEmpty) {
+      return node.daughters.valuesIterator.flatMap(node => getWords(node, prefix)).toStream
+    } else {
+      val newPrefix = Array.concat(prefix, Array(node.letter.get))
+      if (node.frequency > 0 && node.letter.isDefined) {
+        return Stream.cons(newPrefix.mkString, node.daughters.valuesIterator.flatMap(node => getWords(node, newPrefix)).toStream)
+      } else {
+        return node.daughters.valuesIterator.flatMap(node => getWords(node, newPrefix)).toStream
+      }
+    }
+  }
+
+
+  final def getWordFrequencies(node: Node, prefix: Array[Char]): Stream[Tuple2[String, Integer]] = {
+    if (node.letter.isEmpty) {
+      return node.daughters.valuesIterator.flatMap(node => getWordFrequencies(node, prefix)).toStream
+    } else {
+      val newPrefix = Array.concat(prefix, Array(node.letter.get))
+      if (node.frequency > 0 && node.letter.isDefined) {
+        return Stream.cons(new Tuple2(newPrefix.mkString, node.frequency), node.daughters.valuesIterator.flatMap(node => getWordFrequencies(node, newPrefix)).toStream)
+      } else {
+        return node.daughters.valuesIterator.flatMap(node => getWordFrequencies(node, newPrefix)).toStream
+      }
+    }
+  }
+
+
   @tailrec final def contains(node: Node, word : Array[Char]): Boolean = {
     if (word.isEmpty)
-      node.daughters.contains('.')
+      node.frequency > 0
     else {
       if (node.daughters.contains(word.head)) contains(node, word.tail) else false
     }
   }
-  
+
+
   def getDictionary(): Node = {
     new Node()
   }
